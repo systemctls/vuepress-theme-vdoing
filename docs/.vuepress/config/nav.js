e413125bb80ce560a555f990a1b48a1628c44517
jinxm <mr.jinxm@foxmail.com>
2021-06-30 10:54:52 +0800

    upd: 初始版本-大纲搭建

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -2,7 +2,7 @@
 module.exports = [
   { text: '首页', link: '/' },
   {
-    text: '前端',
+    text: '技术栈',
     link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
     items: [
       // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
@@ -42,7 +42,7 @@ module.exports = [
     ],
   },
   {
-    text: '页面',
+    text: '大前端',
     link: '/ui/',
     items: [
       { text: 'HTML', link: '/pages/8309a5b876fc95e3/' },
@@ -50,7 +50,7 @@ module.exports = [
     ],
   },
   {
-    text: '技术',
+    text: '实用文档',
     link: '/technology/',
     items: [
       { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
@@ -69,10 +69,10 @@ module.exports = [
       { text: '实用技巧', link: '/pages/baaa02/' },
     ],
   },
-  { text: '关于', link: '/about/' },
+  // { text: '关于', link: '/about/' },
   {
-    text: '收藏',
-    link: '/pages/beb6c0bd8a66cea6/',
+    text: '收藏夹',
+    link: '/collect/',
     // items: [
     //   { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
     //   { text: '资源', link: '/pages/eee83a9211a70f9d/' },
